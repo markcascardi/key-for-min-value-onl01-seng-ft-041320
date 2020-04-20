0f46f3a4423bda7d7526fbb66f5106d2a0b4e651
Mark Cascardi <mcascardi@gmail.com>
2020-04-20 06:09:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,5 @@
 def key_for_min_value(products)
   return nil if products.empty?
 
-  products.reduce { |min, price| min.last > price.last ? price : min }.first
+  products.reduce { |min_price, current_price| min_price.last > current_price.last ? current_price : min_price }.first
 end
