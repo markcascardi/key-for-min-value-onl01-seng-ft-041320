61110548a1d2ea6fea3dce751354fb9ea925e572
Mark Cascardi <mcascardi@gmail.com>
2020-04-20 06:24:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,3 @@ def key_for_min_value(products)
 
   products.reduce { |min, current| min.last > current.last ? current : min }.first
 end
-
-products = {:chair => 25, :table => 85, :mattress => 450}
-puts key_for_min_value(products)
