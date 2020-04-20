ae8d5c1a89e1490568952bc05ea3ff76c346aa64
Mark Cascardi <mcascardi@gmail.com>
2020-04-20 06:07:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,5 @@
-def key_for_min_value(ikea)
-  return nil if ikea.empty?
+def key_for_min_value(products)
+  return nil if products.empty?
 
-  ikea.reduce { |furniture, price| furniture.last > price.last ? price : furniture }.first
+  products.reduce { |item, price| furniture.last > price.last ? price : furniture }.first
 end
