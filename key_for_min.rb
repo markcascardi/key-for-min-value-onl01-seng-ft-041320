99dd7331bee628c0d70285546f23add8877cac98
Mark Cascardi <mcascardi@gmail.com>
2020-04-20 06:14:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,5 +3,5 @@ require 'pry'
 def key_for_min_value(products)
   return nil if products.empty?
 
-  products.reduce { |item, price| binding.pry item.last > price.last ? price : item }.first
+  products.reduce { |item, price| binding.pry; item.last > price.last ? price : item }.first
 end
